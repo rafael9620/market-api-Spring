89d78df771337dccc65bfd5d10b6bfbe7d796af9
Francisco R <franciscoreg9620@gmail.com>
2021-12-19 12:39:30 +0100

    Mapeadas las clases a entidades.

---
+++ src/main/java/com/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,49 @@
+package com.market.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProdutoPK id;
+
+    private Integer cantidad;
+    private Double  total;
+    private Boolean estado;
+
+    public ComprasProdutoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProdutoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
